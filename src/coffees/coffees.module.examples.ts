91a8f396288951c9ef7effd04b5bc9dfc8d2c1d2
Patryk.budnicki <patryk.budnicki@siili.com>
2022-11-12 17:46:30 +0100

    feat: Dependency Injection

--- src/coffees/coffees.module.examples.ts
+++ src/coffees/coffees.module.examples.ts
@@ -1,3 +1,4 @@
+// It's example file with a lot of comments about different approaches
 import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
@@ -15,12 +16,40 @@ export class CoffeeBrandsFactory {
   }
 }
 
+// Our mock implementation
+// if we want to use mock service, then we need to pass it in useValue property
+// export class MockCoffeesService {}
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Event, Flavor])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
+  // providers: [CoffeesService],
+  // providers: [
+  //   {
+  //     provide: CoffeesService,
+  //     useValue: new MockCoffeesService(), // <-- mock implementation
+  //   }
+  // ],
   providers: [
     CoffeesService,
     CoffeeBrandsFactory,
+    // "useClass" syntax example
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+    // String-valued token // ask someone from project, when we can use it
+    // {
+    //   provide: COFFEE_BRANDS, // 👈
+    //   useValue: ['buddy brew', 'nescafe'], // array of coffee brands,
+    // },
+    // "useFactory" syntax example
     {
       provide: COFFEE_BRANDS,
       useFactory: async (connection: Connection): Promise<string[]> => {
@@ -30,8 +59,10 @@ export class CoffeeBrandsFactory {
         return coffeeBrands;
       },
       inject: [Connection],
+      // useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+      // inject: [CoffeeBrandsFactory]
     },
   ],
-  exports: [CoffeesService], // bez tego nie mógłbym zaimportować tego serwisu w innym module, no chyba ze uzylbym useFactor, prawda?
+  exports: [CoffeesService], // bez tego nie mógłbym zaimportować tego serwisu w innym module
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { DataSource, Repository } from 'typeorm';
@@ -7,6 +7,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor-entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Injectable()
 export class CoffeesService {
@@ -16,7 +17,12 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly dataSource: DataSource,
-  ) {}
+    // something like enums
+    // Injecting string-valued token into CoffeesService
+    @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+  ) {
+    console.log(coffeeBrands);
+  }
 
   findAll(paginationQuery: PaginationQueryDto) {
     // z takim rozwiązaniem nie da się dostać tabeli flavors, ze względu na to ze relacja nie jest eager
