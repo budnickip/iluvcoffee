1814f6ca9abfd5aed7ae045c0b6afd2f7b40847c
Patryk.budnicki <patryk.budnicki@siili.com>
2023-01-03 22:11:50 +0100

    feat: catch exceptions with filters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,10 +13,12 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+//UsePipes() - w tym miejscu wywołany, będzie dla całego modułu
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  //UsePipes() - w tym miejscu wywołany, będzie dla danej metody/routy
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     // const { limit, offset } = paginationQuery;
@@ -33,6 +35,8 @@ export class CoffeesController {
     return this.coffeesService.create(createCoffeeDto);
   }
 
+  // @Body(ValidationPipe) - w miejscu body jakbyśmy tak dali, to wtedy pipe jest tylko dla danego parametru
+  // taki scope jest dostępny tylko dla pipeów
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeDto);
